22aafd6eb986d0a3acad64043999b833cb017aa8
Vladyslava Korovaina <vladyslava.korovainaya@gmail.com>
2025-01-06 20:25:05 +0000

    add task solution

--- src/components/Sum/Sum.spec.jsx
+++ src/components/Sum/Sum.spec.jsx
@@ -1,37 +1,42 @@
-// cypress/integration/Sum.spec.jsx
+// cypress/integration/sum_spec.js
 import React from 'react';
 import { mount } from '@cypress/react18';
-// eslint-disable-next-line import/no-unresolved, import/extensions
-import { Sum } from '../src/components/Sum'; // Переконайтеся, що правильно вказуєте шлях
+import { Sum } from './Sum'; // Іменований імпорт компонента Sum
 
-describe('Sum component tests', () => {
-  it('should correctly sum positive numbers', () => {
-    mount(<Sum a={2} b={3} />);
-    cy.get('p').should('have.text', 'Sum of 2 and 3 is 5');
+describe('Sum', () => {
+  it('should sum positive numbers', () => {
+    mount(<Sum a={7} b={10} />); // Рендеримо компонент з a=7, b=10
+
+    cy.get('p').should('have.text', 'Sum of 7 and 10 is 17'); // Перевіряємо текст
   });
 
   it('should sum positive and negative numbers', () => {
-    mount(<Sum a={6} b={-4} />);
-    cy.get('p').should('have.text', 'Sum of 6 and -4 is 2');
+    mount(<Sum a={6} b={-4} />); // Рендеримо компонент з a=6, b=-4
+
+    cy.get('p').should('have.text', 'Sum of 6 and -4 is 2'); // Перевіряємо текст
   });
 
-  it('should sum only positive numbers', () => {
-    mount(<Sum a={5} b={3} />);
-    cy.get('p').should('have.text', 'Sum of 5 and 3 is 8');
+  it('should sum negative numbers', () => {
+    mount(<Sum a={-40} b={-2} />); // Рендеримо компонент з a=-40, b=-2
+
+    cy.get('p').should('have.text', 'Sum of -40 and -2 is -42'); // Перевіряємо текст
   });
 
-  it('should handle missing b and default to 0', () => {
-    mount(<Sum a={5} />);
-    cy.get('p').should('have.text', 'Sum of 5 and 0 is 5');
+  it('should sum "a" with 0 if "b" is not given', () => {
+    mount(<Sum a={6} />); // Рендеримо компонент з a=6 (b не передано)
+
+    cy.get('p').should('have.text', 'Sum of 6 and 0 is 6'); // Перевіряємо текст
   });
 
-  it('should handle missing a and default to 0', () => {
-    mount(<Sum b={4} />);
-    cy.get('p').should('have.text', 'Sum of 0 and 4 is 4');
+  it('should sum 0 with "b" if "a" is not given', () => {
+    mount(<Sum b={12} />); // Рендеримо компонент з b=12 (a не передано)
+
+    cy.get('p').should('have.text', 'Sum of 0 and 12 is 12'); // Перевіряємо текст
   });
 
-  it('should handle missing a and b, and default to 0', () => {
-    mount(<Sum />);
-    cy.get('p').should('have.text', 'Sum of 0 and 0 is 0');
+  it('should sum 0 with 0 if "a" and "b" are not given', () => {
+    mount(<Sum />); // Рендеримо компонент без параметрів
+
+    cy.get('p').should('have.text', 'Sum of 0 and 0 is 0'); // Перевіряємо текст
   });
 });
