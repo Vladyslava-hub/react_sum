6f011b99c60bf7cd7e35f9a700cc7fcb2eb5e8dc
Vladyslava Korovaina <vladyslava.korovainaya@gmail.com>
2025-01-07 11:58:30 +0000

    fixed task

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-// src/App.jsx
 import React from 'react';
 import './App.scss';
 // eslint-disable-next-line import/no-unresolved, import/extensions
@@ -8,8 +7,8 @@ export const App = () => (
   <>
     <Sum a={2} b={3} />
     <Sum a={-5} b={5} />
-    <Sum a={10} b={0} />
-    <Sum a={0} b={5} />
-    <Sum a={0} b={0} />
+    <Sum a={10} />
+    <Sum b={5} />
+    <Sum />
   </>
 );
--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,5 @@
 import { createRoot } from 'react-dom/client';
 
-// eslint-disable-next-line import/named
 import { App } from './App';
 
 createRoot(document.getElementById('root')).render(<App />);
